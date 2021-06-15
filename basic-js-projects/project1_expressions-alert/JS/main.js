43841a13d76bb1cd2700188ce4e0048d6ad9ca1f
EAMONN PRUNTY <EAMONNPRUNTY101@GMAIL.COM>
2021-06-15 14:14:07 -0500

    step 66 JS course

--- basic-js-projects/project1_expressions-alert/JS/main.js
+++ basic-js-projects/project1_expressions-alert/JS/main.js
@@ -1,1 +1,14 @@
-alert("hello, world");
+alert("hello, world"); //creating alert box
+var a = "this is a string"; //creating variable
+document.write(a) //printing above variable
+document.write("Holy crap, \"Stop it\""); //escaped charachters
+document.write("concat"+"enate"); //concatenated string
+var dinner = "pasta", protein = "meatballs", sauce ="pasta sauce"; //declared multiple vars
+document.write(sauce) //print "pasta sauce"
+var dinner=dinner.fontcolor("blue"); //change color to blue
+document.write(dinner); //print " in blue"
+var protein=protein.fontcolor("brown"); //change color to brown
+document.write(protein); //print "meatballs in  brown"
+var sauce=sauce.fontcolor("red"); //change color to red
+document.write(sauce); //print "pasta sauce" in red
+document.write(8*60); //expression//
